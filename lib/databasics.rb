cdd9753505e2214072cacafde4ce7e6cceca1cc1
Jennifer Ford <jenncford@gmail.com>
2016-03-23 04:54:04 -0400

    Added new classes App, Items and Oders

--- lib/databasics.rb
+++ lib/databasics.rb
@@ -5,11 +5,31 @@ require "pry"
 require "databasics/version"
 require "databasics/user"
 require "databasics/address"
+require "databasics/items"
+require "databasics/orders"
 
 db_config = YAML.load(File.open("config/database.yml"))
 ActiveRecord::Base.establish_connection(db_config)
 
 module Databasics
+  class App < ActiveRecord::Base
+    def create_new_user
+    end
+
+    def display_users_current_address
+    end
+
+    def user_order_history
+#items bought and quantity
+    end
+
+    def new_order
+      #prompt for item name, quantity
+      #place new order
+      #return message if item not found
+    end
+  end
+
   # Your code goes here...
 
   ## 3 ActiveRecord querying methods
