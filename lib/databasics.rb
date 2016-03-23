77ff27e6aff895af775b969b7e6bedef661fe7b7
Jennifer Ford <jenncford@gmail.com>
2016-03-23 08:47:34 -0400

    Commented outline

--- lib/databasics.rb
+++ lib/databasics.rb
@@ -14,9 +14,20 @@ ActiveRecord::Base.establish_connection(db_config)
 module Databasics
   class App < ActiveRecord::Base
     def create_new_user
+      puts "What is your first name"
+      first_name = gets.chomp.to_s
+      puts "What is your last name"
+      last_name gets.chomp.to_s
+      puts "Whats your email"
+      email = gets.chomp.to_s
+      new_user=User.find_or_create_by(first_name: first_name,last_name: last_name
+      email: email)
+      new_user.save
+      binding.pry
     end
 
     def display_users_current_address
+      #probably by using client = Client.last for this one
     end
 
     def user_order_history
@@ -26,14 +37,16 @@ module Databasics
     def new_order
       #prompt for item name, quantity
       #place new order
-      #return message if item not found
+      #return message if item not found probably use find_by!
     end
-  end
-
-  # Your code goes here...
-
   ## 3 ActiveRecord querying methods
   # 1) Model.where
   # 2) Model.find
   # 3) Model.find_by
 end
+
+app = Databasics::App.new
+app.create_new_user
+app.display_users_current_address
+app.user_order_history
+app.new_order
